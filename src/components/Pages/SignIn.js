639e6268c95431e727c87509227338e977a80c4e
Zoran G <68257343+verona-hub@users.noreply.github.com>
2022-03-11 01:53:11 +0100

    - changed css for the LogIn component - added form and inputs for LogIn component

--- src/components/Pages/SignIn.js
+++ src/components/Pages/SignIn.js
@@ -5,7 +5,33 @@ const SignIn = () => {
     return (
         <div className='SignIn'>
             <h1> Sign in </h1>
-            <button> Login </button>
+            <form>
+
+                <input
+                    placeholder='Email'
+                    type='email'
+                />
+
+                <input
+                    placeholder='Password'
+                    type='password'
+                />
+
+                <input
+                    type="checkbox"
+                    id="vehicle3"
+                    name="vehicle3"
+                    value="Boat"
+                />
+                <label htmlFor="vehicle3"> Remember me </label>
+
+                <input
+                    className='button-login'
+                    type="button"
+                    value="Login"
+                />
+
+            </form>
         </div>
     );
 };
